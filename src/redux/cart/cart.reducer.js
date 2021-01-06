cf3e2cfa109107b848f74c625e3798b505833c32
drewhinde <drewhinde@hotmail.com>
2021-01-06 11:41:25 +0000

    Added Quantity to Cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import { cartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
-const INITIAL_STATE = { hidden: true };
+const INITIAL_STATE = { hidden: true, cartItems: [] };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
@@ -8,6 +9,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case cartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
